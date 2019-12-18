eeb60938faf41d246a4a84c6742dbab98dc6921d
Din Sebastian <dinsebi22@gmail.com>
2019-12-18 10:33:26 +0200

    Add files via upload

---
+++ Rain/index.js
@@ -0,0 +1,99 @@
+let cnvs = document.getElementById("cnvs");
+cnvs.width = window.innerWidth;
+cnvs.height = window.innerHeight;
+let ctx = cnvs.getContext('2d');
+
+let rain = [];
+
+function Drop(x, y, maxDist) {
+    this.x = x;
+    this.y = y;
+    this.maxDist = maxDist;
+    this.dropSpeed = Math.random() * 5 + 7
+
+    this.draw = function () {
+
+        if (this.y >= this.maxDist) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+            ctx.ellipse(this.x, this.y, 4, 2, 0, 0, 2 * Math.PI);
+            ctx.fillStyle = 'rgba(255,255,255,0.8)';
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+
+            this.x = Math.random() * window.innerWidth - Math.random() * 100;
+            this.y = -10;
+        } else {
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x + 1, this.y + this.dropSpeed)
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "white"
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
+    this.move = function () {
+        this.x += 1;
+        this.y += this.dropSpeed;
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    street();
+    streetLine();
+
+    for (let i = 0; i < rain.length; i++) {
+        rain[i].move();
+        rain[i].draw();
+    }
+
+}
+
+
+function street() {
+    var grd = ctx.createLinearGradient(0, window.innerHeight, window.innerWidth, window.innerHeight);
+      
+    // Add colors
+    grd.addColorStop(0.000, 'rgba(0, 0, 0, 1.000)');
+    grd.addColorStop(0.974, 'rgba(58, 58, 58, 1.000)');
+
+    // ctx.beginPath();
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, window.innerHeight - 210, window.innerWidth, window.innerHeight);
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+
+function streetLine() {
+    let offset = 20;
+
+    for (let i = 0; i < 8; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = 'rgb(211,211,211)';
+        ctx.moveTo(offset, window.innerHeight - 150);
+        ctx.lineTo(offset + 150, window.innerHeight - 150);
+        ctx.lineTo(offset + 100, window.innerHeight - 130);
+        ctx.lineTo(offset - 50, window.innerHeight - 130);
+        ctx.lineTo(offset, window.innerHeight - 150);
+        ctx.closePath();
+        ctx.fill();
+        offset += 250;
+    }
+
+}
+
+
+function init() {
+    for (let i = 0; i < 2500; i++) {
+        rain.push(new Drop(Math.random() * window.innerWidth - 100, Math.random() * window.innerHeight - window.innerHeight, window.innerHeight - Math.random() * 200));
+    }
+}
+
+init();
+animate();
